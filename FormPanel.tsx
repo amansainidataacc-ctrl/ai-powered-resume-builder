fe891a619cf45335f189b4d437ea0f43b23bd964
amansainidataacc-ctrl <amansaini.data.acc@gmail.com>
2026-04-03 16:43:42 +0530

    Fix import paths by removing the './sections/' subdirectory references

---
+++ FormPanel.tsx
@@ -0,0 +1,7 @@
+import PersonalSection from './PersonalSection';
+import SummarySection from './SummarySection';
+import ExperienceSection from './ExperienceSection';
+import EducationSection from './EducationSection';
+import SkillsSection from './SkillsSection';
+import ProjectsSection from './ProjectsSection';
+import CertificationsSection from './CertificationsSection';
